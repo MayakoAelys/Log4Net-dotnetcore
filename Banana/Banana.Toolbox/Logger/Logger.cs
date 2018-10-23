28206e831d907867310dd78f7cd90dc67478d0fb
Lyyn <lyynou@gmail.com>
2018-10-23 15:42:26 +0200

    Added example files

---
+++ Banana/Banana.Toolbox/Logger/Logger.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using log4net;
+using log4net.Config;
+using log4net.Repository;
+
+namespace Banana.Toolbox.Logger
+{
+    public static class Logger
+    {
+        private static ILog _log;
+
+        public static void Log(string message)
+        {
+            EnsureLogger();
+
+            _log.Debug($"Test log: {message}");
+        }
+
+        private static void EnsureLogger()
+        {
+            if (_log != null) return;
+
+            var assembly = Assembly.GetEntryAssembly();
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            var configFile = GetConfigFile();
+
+            // Configure Log4Net
+            XmlConfigurator.Configure(logRepository, configFile);
+            _log = LogManager.GetLogger(assembly, assembly.ManifestModule.Name.Replace(".dll", ""));
+        }
+
+        private static FileInfo GetConfigFile()
+        {
+            FileInfo configFile = null;
+
+            // Search config file
+            var configFileNames = new[] { "Config/log4net.config", "log4net.config" };
+
+            foreach (var configFileName in configFileNames)
+            {
+                configFile = new FileInfo(configFileName);
+
+                if (configFile.Exists) break;
+            }
+
+            if (configFile == null) throw new NullReferenceException("Log4net config file not found.");
+
+            return configFile;
+        }
+    }
+}
---
+++ Banana/Banana/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Banana.Toolbox.Logger;
+
+namespace Banana
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Logger.Log("Message from the main program");
+        }
+    }
+}
